08789a105031c426d68ba3c33889e791bc8746f1
rociojalil <rojalilparedes@gmail.com>
2021-09-01 01:22:26 -0300

    ahora si funciona busqueda x query

--- api/src/routes/PokeRoutes/pokemons.js
+++ api/src/routes/PokeRoutes/pokemons.js
@@ -4,23 +4,18 @@ const { Router }  = require ('express');
 const { conn, Pokemon, Types } = require('../../db');
 const router = Router();
 
-// [ ] GET /pokemons + GET /pokemons?name="..."
+// [ ] GET /pokemons (40) + GET /pokemons?name="..." (búsqueda x query)
+// podría hacer que en el splice me traiga menos... 9
 
 let store = []
 
-//Deja todo a 0 - refresca tablas
-router.put("/", (req, res) => {
-    store = []  //limpia el buffer
-    conn.sync({ force: true }).then(() => res.send("resfresh exitoso")) 
-})
-
-// Trae 40 pokemones pero los envia de a 9 cada vez que piden
 router.get("/", async function (req, res) {
 
     const { q } = req.query
 
     // si la búsqueda no es por query --> trae los 40 de API
         if (!q) {
+         
             let array = []
             //recorro hasta 40 poke
             for (let i = 1; i <= 40; i++) {
@@ -28,7 +23,7 @@ router.get("/", async function (req, res) {
 
             await Promise.all(array)
                 .then(resolve => {
-                    // me traigo lo que quiero de api y lo almaceno en store
+                    // me traigo lo que quiero de api y lo almaceno en store []
                     resolve.forEach((result => {
                         let pokemon = result.data
                         store.push({
@@ -40,53 +35,36 @@ router.get("/", async function (req, res) {
                             fuerza: pokemon.stats[1].base_stat
                         })   
                     }))
-                }).catch(err => next(err))
-         }
-        //  acá es donde le digo que me muestre 9
-        res.json({ nuevePoke: store.splice(0, 9), cuantosQuedan: store.length })
-
-     if (q) {  // búsqueda x query. Me traigo los de mi BD si no API ---> ver si trae exacto...
-
-        let pokeDb = await Pokemon.findAll({
-            where: { name: q }, include: Types
-        })
-
-        if (pokeDb.length !== 0) {
-
-            pokeDb = pokeDb[0].dataValues
-            res.json({
-                name: pokeDb.name,
-                types: pokeDb.Types.map((e) => e.type),
-                fuerza: pokeDb.fuerza,
-                imagen: pokeDb.imagen,
-                id: pokeDb.id
-            })
+                }).catch(error => json({ error: "Ups!" }))
+         
+        //  acá es donde le digo que me muestre 40
+        res.json({cuarentaPoke: store.splice(0, 40), cuantosQuedan: store.length });
         }
 
-        else { //desde la API (mismo que antes)
+        else if (q) {  //búsqueda x query exacta --> trae info desde api
 
             axios.get("https://pokeapi.co/api/v2/pokemon/" + q)
                 .then(response => {
+
                     let pokemon = response.data
+
                     res.json({
                         name: pokemon.name,
                         types: pokemon.types.map((e) => e.type.name),
                         imagen: pokemon.sprites.other["official-artwork"].front_default,
                         fuerza: pokemon.stats[1].base_stat,
                         id: pokemon.id
+
                     })
                 })
-                // VER TEMA DE CATCH
+
                 .catch(() => res.json(
                     {
-                        name: "Not found",
-                        types: [],
-                        fuerza: "",
-                        id: "",
-                        imagen: "https://pm1.narvii.com/6121/2985db5e175084c069f3cab12a9afb5a896ee276_hq.jpg"
+                        name: "Not Found",
+                        imagen: "https://i1.wp.com/hipertextual.com/wp-content/uploads/2020/05/hipertextual-nuevo-pokemon-go-es-hacer-que-tus-pokemon-se-vean-mas-reales-2020697065.jpg?fit=1200%2C771&ssl=1"
+                        // imagen default de no encontrar poke x query ---> ver desp en front
                     }
                 ))
-        }
     }
 })
 
